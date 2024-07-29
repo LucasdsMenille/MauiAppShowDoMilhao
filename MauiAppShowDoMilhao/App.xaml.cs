7727b5fa7d12857e8b14066723e06feabc5f1270
Usuario <Usuario@CPD03-M13>
2024-07-29 15:08:21 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppShowDoMilhao/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MauiAppShowDoMilhao.Models;
+
+namespace MauiAppShowDoMilhao;
+
+public partial class App : Application
+{
+    List<Pergunta> perguntas_faceis = new()
+    {
+        new Pergunta
+        {
+            Id = 1,
+            Enunciado = "Qual o nome dado ao estado da água em forma de gelo?",
+            Alternativas = new()
+            {
+                new Alternativa { Correta = false, Descricao = "LÍQUIDO" },
+                new Alternativa { Correta = true, Descricao = "SÓLIDO" },
+                new Alternativa { Correta = false, Descricao = "GASOSO" },
+                new Alternativa { Correta = false, Descricao = "VAPOROSO" }
+            }
+
+        },
+        new Pergunta
+        {
+            Id = 2,
+            Enunciado = "Quem a namorada do Mickey?",
+            Alternativas = new()
+            {
+                new Alternativa { Correta = false, Descricao = "MARGARIDA" },
+                new Alternativa { Correta = true, Descricao = "MINNIE" },
+                new Alternativa { Correta = false, Descricao = "A PEQUENA SEREIA" },
+                new Alternativa { Correta = false, Descricao = "OLÍVIA PALITO" }
+            }
+        },
+        new Pergunta
+        {
+            Id = 3,
+            Enunciado = "Qu?",
+            Alternativas = new()
+            {
+                new Alternativa { Correta = false, Descricao = "MARGARIDA" },
+                new Alternativa { Correta = true, Descricao = "MINNIE" },
+                new Alternativa { Correta = false, Descricao = "A PEQUENA SEREIA" },
+                new Alternativa { Correta = false, Descricao = "OLÍVIA PALITO" }
+            }
+        },
+    };
+}
+
+
